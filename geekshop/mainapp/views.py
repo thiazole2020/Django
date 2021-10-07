df00ed6596546a4058c96593e22b6bfe22adaab6
Anshin Vitaly <thiazole@bk.ru>
2021-10-07 19:41:06 +0300

    DZ_2/AnshinVS

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+import json
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    title = 'Каталог'
+
+    f = open('mainapp\data.json', 'r', encoding='utf8')
+    links_menu = json.load(f)
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
+
+    return render(request, 'mainapp/products.html', context)
